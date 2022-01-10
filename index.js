b4f2161b19a2c00d8f166936f226efb047d94d97
JonathanLaureano <JonathanLaureano20@gmail.com>
2022-01-09 23:43:04 -0500

    commit tree dbf8befac48c0351af614938a008c0920e9fd793

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let result = ''
+  if (feet <= 400){
+    result = 'This one is on me!'
+  }
+  else if ((feet > 2000) && (feet < 2500)){
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else if (feet > 2500){
+    result = 'No can do.'
+  }    
+  return result
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === "generous"){
+      return "Thank you so much."
+    } else if (tip === "not as generous"){
+      return "Thank you."
+    } else if (tip === "thanks for everything"){
+      return "Bye."
+    }
 }
